52c813f0cfec420dc63c21d03bc50e459c306614
AP-Porag <smauhp@gmail.com>
2021-12-02 19:35:58 +0600

    Trail One

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -18,7 +18,8 @@
 
     <!-- ======= Contact Section ======= -->
     <div class="map-section">
-      <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+      <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1822.7836899797564!2d90.44266343305927!3d23.975733632177732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755daa3c71ccead%3A0x4c079e6aa63b4719!2sNilerpara%20-%20Kanaiya%20Rd!5e0!3m2!1sen!2sbd!4v1638451475750!5m2!1sen!2sbd" allowfullscreen="" loading="lazy"></iframe>
+<!--      <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>-->
     </div>
 
     <section id="contact" class="contact">
